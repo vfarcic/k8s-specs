6fb1d6a2f24f6ddae1656bf6214e370e793bc526
neerajkothari <neeraj.kothari@gmail.com>
2018-05-11 14:57:24 -0400

    master.sh correction

--- network/basic/master.sh
+++ network/basic/master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Initializing kubernetes master..."
-sudo kubeadm init --apiserver-advertise-address 10.100.198.200 --pod-network-cidr 10.244.0.0/16
+sudo kubeadm init --apiserver-advertise-address 10.100.198.200
 sudo kubeadm token create --print-join-command > /vagrant/nodetoken.out
 sudo cp /etc/kubernetes/admin.conf /vagrant
