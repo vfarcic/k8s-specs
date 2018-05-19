7e6e1ee969f05981f20026423a5b0701f049a69e
neerajkothari <neeraj.kothari@gmail.com>
2018-05-18 21:04:36 -0400

    network cluster changes

--- network/cluster/bootstrap.sh
+++ network/cluster/bootstrap.sh
@@ -4,4 +4,4 @@ echo "Installing..."
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y docker-engine kubelet kubeadm kubectl
+sudo apt-get install -y docker.io kubelet kubeadm kubectl
