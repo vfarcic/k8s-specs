e44236f32857942822e791a44ac976cc3d165639
Gonzalo <gonzalom.lopez@softtek.com>
2018-06-11 07:07:12 +0000

    ADD kubeadm-DO flavor: Include all terraform and packer definitions to bring up a k8s cluster on DO

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,4 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
+provider "digitalocean" {
+  token = "${var.token}"
 }
-
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,142 +1,51 @@
-data "aws_ami" "k8s" {
-  filter {
-    name   = "name"
-    values = ["ami-kubernetes-*"]
-  }
-  most_recent = true
-
-  owners = ["self"]
+resource "digitalocean_ssh_key" "k8s_key" {
+  name = "k8s_key"
+  public_key = "${file("k8s-key.pub")}"
 }
 
-data "template_file" "init" {
-  template = "${file("init.tpl")}"
+resource "digitalocean_droplet" "k8s_master" {
+  name     = "k8s-master"
+  image    = "${var.k8s_snapshot_id}"
+  region   = "nyc3"
+  size     = "s-2vcpu-2gb"
+  ssh_keys = ["${digitalocean_ssh_key.k8s_key.id}"]
 
-  vars {
-    pod_network_cidr = "10.244.0.0/16"
-  }
-}
+  provisioner "remote-exec" {
+    inline = [
+      "sysctl net.bridge.bridge-nf-call-iptables=1",
+      "kubeadm init --pod-network-cidr=\"10.244.0.0/16\" | sudo tee /opt/kube-init.log",
+    ]
 
-data "template_file" "join" {
-  template = "${file("join.tpl")}"
-
-  vars {
-    join_command = "${var.k8s_join_command}"
-  }
-}
-
-resource "aws_eip_association" "eip_assoc" {
-  instance_id   = "${aws_instance.kube-master.id}"
-  allocation_id = "${aws_eip.master-ip.id}"
-}
-
-resource "aws_instance" "kube-master" {
-  ami                    = "${data.aws_ami.k8s.id}"
-  instance_type          = "${var.k8s_master_instance_type}"
-  user_data              = "${data.template_file.init.rendered}"
-  key_name               = "${var.k8s_key_name}"
-  vpc_security_group_ids = ["${aws_security_group.kube-sg.id}"]
-
-  tags {
-    Name    = "k8s-master-${var.k8s_lab_name}"
-    LabName = "${var.k8s_lab_name}"
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = "${file("k8s-key")}"
+    }
   }
 }
 
-resource "aws_instance" "kube-node" {
-  ami                    = "${data.aws_ami.k8s.id}"
-  instance_type          = "${var.k8s_node_instance_type}"
-  user_data              = "${data.template_file.join.rendered}"
-  key_name               = "${var.k8s_key_name}"
-  vpc_security_group_ids = ["${aws_security_group.kube-sg.id}"]
-  count                  = "${var.k8s_nodes}"
+resource "digitalocean_droplet" "k8s_node" {
+  name     = "k8s-node-${count.index + 1}"
+  image    = "${var.k8s_snapshot_id}"
+  region   = "nyc3"
+  size     = "s-2vcpu-2gb"
+  ssh_keys = ["${digitalocean_ssh_key.k8s_key.id}"]
+  count    = "${var.k8s_nodes}"
 
-  tags {
-    Name    = "k8s-node-${var.k8s_lab_name}"
-    LabName = "${var.k8s_lab_name}"
-  }
+  provisioner "remote-exec" {
+    inline = [
+      "sysctl net.bridge.bridge-nf-call-iptables=1",
+      "${var.k8s_join_command}",
+    ]
 
-  lifecycle {
-    ignore_changes = ["user_data"]
-  }
-}
-
-resource "aws_eip" "master-ip" {}
-
-resource "aws_security_group" "kube-sg" {
-  name = "k8s"
-
-  // SSH
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  //  K8S API SERVER 
-  ingress {
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
-    self = true
-  }
-
-  // etcd Sever client API
-  ingress {
-    from_port   = 2379
-    to_port     = 2380
-    protocol    = "tcp"
-    self = true
-  }
-
-  //  K8S kubelet API 
-  ingress {
-    from_port   = 10250
-    to_port     = 10250
-    protocol    = "tcp"
-    self = true
-  }
-
-  //  K8S kubelet API 
-  ingress {
-    from_port   = 10251
-    to_port     = 10251
-    protocol    = "tcp"
-    self = true
-  }
-
-  //  K8S kubelet API 
-  ingress {
-    from_port   = 10252
-    to_port     = 10252
-    protocol    = "tcp"
-    self = true
-  }
-
-  //  K8S kubelet API READ ONLY  
-  ingress {
-    from_port   = 10255
-    to_port     = 10255
-    protocol    = "tcp"
-    self = true
-  }
-
-  //  K8S Node Port Services
-  ingress {
-    from_port   = 30000
-    to_port     = 32767
-    protocol    = "tcp"
-    self = true
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = "${file("k8s-key")}"
+    }
   }
 }
 
 output "master-ip" {
-  value = "${aws_eip.master-ip.public_ip}"
+  value = "${digitalocean_droplet.k8s_master.ipv4_address}"
 }
