21dbcafc436134c2f7279f84d3551ab2b109a306
gmlp <gmlp.24a@gmail.com>
2018-07-03 19:22:42 +0000

    REF: store token in DO-spaces https://github.com/vfarcic/k8s-specs/pull/6

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,25 @@
 resource "digitalocean_ssh_key" "k8s_key" {
-  name = "k8s_key"
+  name       = "k8s_key"
   public_key = "${file("k8s-key.pub")}"
 }
 
 resource "digitalocean_droplet" "k8s_master" {
   name     = "k8s-master"
   image    = "${var.k8s_snapshot_id}"
-  region   = "nyc3"
-  size     = "s-2vcpu-2gb"
+  region   = "${var.region}"
+  size     = "${var.k8s_master_size}"
   ssh_keys = ["${digitalocean_ssh_key.k8s_key.id}"]
+
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = "${file("k8s-key")}"
+  }
+
+  provisioner "file" {
+    source      = "s3cfg.tpl"
+    destination = "~/.s3cfg"
+  }
 
   provisioner "remote-exec" {
     inline = [
@@ -16,35 +27,47 @@ resource "digitalocean_droplet" "k8s_master" {
       "kubeadm init --pod-network-cidr=\"10.244.0.0/16\" | sudo tee /opt/kube-init.log",
       "export KUBECONFIG=/etc/kubernetes/admin.conf",
       "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml",
+      "kubeadm token create --print-join-command > k8s_join_cmd",
+      "AWS_SECRET_ACCESS_KEY=${var.space_secret_key} AWS_ACCESS_KEY_ID=${var.space_access_key} s3cmd put k8s_join_cmd s3://${var.do_space}",
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "AWS_SECRET_ACCESS_KEY=${var.space_secret_key} AWS_ACCESS_KEY_ID=${var.space_access_key} s3cmd rm s3://${var.do_space}/k8s_join_cmd",
     ]
 
-    connection {
-      type        = "ssh"
-      user        = "root"
-      private_key = "${file("k8s-key")}"
-    }
+    when = "destroy"
   }
 }
 
 resource "digitalocean_droplet" "k8s_node" {
   name     = "k8s-node-${count.index + 1}"
   image    = "${var.k8s_snapshot_id}"
-  region   = "nyc3"
-  size     = "s-2vcpu-2gb"
+  region   = "${var.region}"
+  size     = "${var.k8s_node_size}"
   ssh_keys = ["${digitalocean_ssh_key.k8s_key.id}"]
   count    = "${var.k8s_nodes}"
+
+  depends_on = ["digitalocean_droplet.k8s_master"]
+
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = "${file("k8s-key")}"
+  }
+
+  provisioner "file" {
+    source      = "s3cfg.tpl"
+    destination = "~/.s3cfg"
+  }
 
   provisioner "remote-exec" {
     inline = [
       "sysctl net.bridge.bridge-nf-call-iptables=1",
-      "${var.k8s_join_command}",
+      "AWS_SECRET_ACCESS_KEY=${var.space_secret_key} AWS_ACCESS_KEY_ID=${var.space_access_key} s3cmd get s3://${var.do_space}/k8s_join_cmd",
+      "sh k8s_join_cmd",
     ]
-
-    connection {
-      type        = "ssh"
-      user        = "root"
-      private_key = "${file("k8s-key")}"
-    }
   }
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,14 +2,26 @@ variable "token" {
   default = ""
 }
 
-variable "k8s_snapshot_id" {
-  default = ""
+variable "region" {
+  default = "nyc3"
+}
+
+variable "k8s_master_size" {
+  default = "s-2vcpu-2gb"
+}
+
+variable "k8s_node_size" {
+  default = "s-2vcpu-2gb"
 }
 
 variable "k8s_nodes" {
   default = "2"
 }
 
-variable "k8s_join_command" {
-  default = ""
-}
+variable "k8s_snapshot_id" {}
+
+variable "do_space" {}
+
+variable "space_access_key" {}
+
+variable "space_secret_key" {}
