b717e7b3c9197cb8d4ddab9715ad25b3380fe574
Viktor Farcic <vfarcic@farcic.com>
2019-10-30 17:55:13 +0100

    GKE w/Terraform

---
+++ terraform/gke/cluster.tf
@@ -0,0 +1,87 @@
+variable "cluster_name" {
+  type    = string
+  default = "devops-paradox"
+}
+
+variable "project_id" {
+  type    = string
+  default = "devops-26"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east1"
+}
+
+variable "machine_type" {
+  type    = string
+  default = "g1-small"
+}
+
+variable "preemptible" {
+  type    = bool
+  default = true
+}
+
+variable "min_node_count" {
+  type    = number
+  default = 1
+}
+
+variable "max_node_count" {
+  type    = number
+  default = 3
+}
+
+variable "k8s_version" {
+  type = string
+}
+
+provider "google" {
+  credentials = "${file("account.json")}"
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_container_cluster" "primary" {
+  name                     = var.cluster_name
+  location                 = var.region
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  min_master_version       = var.k8s_version
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name         = var.cluster_name
+  location     = var.region
+  cluster      = "${google_container_cluster.primary.name}"
+  version      = var.k8s_version
+  node_count   = 1
+  node_config {
+    preemptible  = var.preemptible
+    machine_type = var.machine_type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+  autoscaling { 
+    min_node_count = var.min_node_count
+    max_node_count = var.max_node_count
+  }
+  management {
+    auto_upgrade = false
+  }
+  timeouts {
+    create = "15m"
+    update = "1h"
+  }
+}
+
+output "cluster_name" {
+  value = var.cluster_name
+}
+
+output "region" {
+  value = var.region
+}
